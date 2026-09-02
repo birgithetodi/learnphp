434000aa5af65e490640597810490f2b81807929
Birgithe Todi <Birgithe.todi@tptlive.ee>
2026-09-02 13:21:00 +0300

    add variable examples

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-echo "Hello, World!";
+echo "Hello, World!"; //writes out "hello, world!" to browser
+// hello
+// world
+
+/**
+ * 
+ * long comment
+ * some cool stuff
+ */
+
+$name = "John Doe"; //variable declaration
+$age = 30; //variable declaration
+echo $age; //writes out 30 to browser
+$age = $age * 2 * 4 * 5 *8;
+echo $age; //writes out 960 to browser
+
+echo  2 + 2; //output the result of 2 + 2
 ?>
