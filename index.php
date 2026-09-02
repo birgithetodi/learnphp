a2dfbf04a7e964c55eb8f8a164a9f73681f0ec56
Birgithe Todi <Birgithe.todi@tptlive.ee>
2026-09-02 13:55:11 +0300

    add types examples

--- index.php
+++ index.php
@@ -1,19 +1,24 @@
 <?php
-echo "Hello, World!"; //writes out "hello, world!" to browser
-// hello
-// world
+$test = 10; //integer
+$test = 4.3; //float
+$test = "Hello World"; //string
+$test = "õpilane";
+$test = "😶‍🌫️";
+$test = true; //boolean
+$test = false; //boolean
+$test = null; //null
+$lol;
+var_dump($test); //var_dump() is used to display the data type and value of a variable
+var_dump($lol); 
 
-/**
- * 
- * long comment
- * some cool stuff
- */
 
-$name = "John Doe"; //variable declaration
-$age = 30; //variable declaration
-echo $age; //writes out 30 to browser
-$age = $age * 2 * 4 * 5 *8;
-echo $age; //writes out 960 to browser
+$test = array (1, 2, "three", true);
+var_dump($test);
 
-echo  2 + 2; //output the result of 2 + 2
-?>
+$test = new stdClass();
+$test->name = "biku";
+var_dump($test);
+
+$num1 = 10;
+$num2 = 10;
+var_dump($num1 === $num2); //=== equal
