c2c036f4675acd1d3c6fbc979360719d03d61a5e
Birgithe Todi <birgithe.todi@tptlive.ee>
2026-09-04 13:50:48 +0300

    add boolean math

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php
-$test = 'hello';
-$test = 'hello' . 'world'; //. combines texts
-$test .= '!';
-$name = 'Birgithe';
-$age = 18;
-$test = $name . 'is' . $age . 'years old';
-$test = "$name is $age years old";
-$test = "hello\n\nworld";
-$letter = 'a';
-$letter++; //++ increases a 1 time makeing it b
-var_dump($letter);
+$test = true;
+$test = false;
+$test = true && false; //only time to get true is true && true, anything else is false
+$test = true || false; //its true when 1 is atleast true
+$test = !false; //! is not
+
+$test = 10 > 5;
+$test = 10 < 5;
+$test = 10 < 10;
+$test = 10 == 10; //
+$test = 10 === 10; //
+$test = 10 >= 5;
+$test = 10 <= 5;
+$test = 10 != 5; 
+$test = 10 !== 5; //not equal
+$test = 10 == '10'; //nr and text
+$test = 10 === '10';
+$test = true && true || (true && !true);
+
+var_dump($test);
