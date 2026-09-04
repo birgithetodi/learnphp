3e20739a569966ad6e3f9df20220226385b1ac1c
Birgithe Todi <birgithe.todi@tptlive.ee>
2026-09-04 14:06:26 +0300

    add array examples

--- index.php
+++ index.php
@@ -1,21 +1,33 @@
 <?php
-$test = true;
-$test = false;
-$test = true && false; //only time to get true is true && true, anything else is false
-$test = true || false; //its true when 1 is atleast true
-$test = !false; //! is not
-
-$test = 10 > 5;
-$test = 10 < 5;
-$test = 10 < 10;
-$test = 10 == 10; //
-$test = 10 === 10; //
-$test = 10 >= 5;
-$test = 10 <= 5;
-$test = 10 != 5; 
-$test = 10 !== 5; //not equal
-$test = 10 == '10'; //nr and text
-$test = 10 === '10';
-$test = true && true || (true && !true);
-
+$test = [1, 2, 'word', true];
+var_dump($test[2]); //[nr] writes down only one thing from the array
+$test[2] = asdhas;
 var_dump($test);
+$test[4] = 'new value';
+var_dump($test);
+$test[] = 'another value';
+var_dump($test);
+array_push($test, 'pushed value');
+var_dump($test);
+
+$test = [ "name " => 'Birgithe', 'age' => 18];
+var_dump($test['name']);
+var_dump($test);
+
+$test = [ 
+    "name " => 'Birgithe', 
+    'age' => 18,
+    1,
+    3,
+    'bdasihbdhajs',
+    100=> 'new value',
+    5,
+];
+var_dump($test['name']);
+var_dump($test);
+$test = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+var_dump($test[1][2]); 
