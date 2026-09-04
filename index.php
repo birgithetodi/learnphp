fbaa632bca7513f4c5c6ccc0a4911948418e1a6b
Birgithe Todi <birgithe.todi@tptlive.ee>
2026-09-04 13:06:56 +0300

    add math functions

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
 <?php
 $test = 10; //integer
-$test = 4.3; //float
-$test = "Hello World"; //string
-$test = "õpilane";
-$test = "😶‍🌫️";
-$test = true; //boolean
-$test = false; //boolean
-$test = null; //null
-$lol;
-var_dump($test); //var_dump() is used to display the data type and value of a variable
-var_dump($lol); 
+$test = 10 + 2;
+$test = 10 - 2;
+$test = 10 * 2;
+$test = 10 / 2;
+$test = $test -1;
 
+$test -= 1; //*, +, -, /
+$test /= 1; 
+$test += 1; 
+$test *= 1;
 
-$test = array (1, 2, "three", true);
+$test = 10 % 3;
+$test++; //$test += 1; is the same thing
+$test--;
+
+$test = 10 + 2 - 3 * 2 / 4;
+$test =(10 + (2 - 3) * 4) / 2;
+$test %= 2;
+$test **= 3;
+$test = 2 ** 4;
+
+$test =  sqrt(16);
 var_dump($test);
-
-$test = new stdClass();
-$test->name = "biku";
-var_dump($test);
-
-$num1 = 10;
-$num2 = 10;
-var_dump($num1 === $num2); //=== equal
