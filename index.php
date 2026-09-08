d8165c7e90508f7c89fd90c59ed130a31e90e279
Birgithe Todi <Birgithe.todi@tptlive.ee>
2026-09-08 11:13:58 +0300

    add if and switch examples

--- index.php
+++ index.php
@@ -1,33 +1,38 @@
 <?php
-$test = [1, 2, 'word', true];
-var_dump($test[2]); //[nr] writes down only one thing from the array
-$test[2] = asdhas;
-var_dump($test);
-$test[4] = 'new value';
-var_dump($test);
-$test[] = 'another value';
-var_dump($test);
-array_push($test, 'pushed value');
-var_dump($test);
+$num = 10;
+if($num > 5) {
+    var_dump('Bigger');
+}
 
-$test = [ "name " => 'Birgithe', 'age' => 18];
-var_dump($test['name']);
-var_dump($test);
+if($num > 5) {
+    var_dump('Bigger');
+} else {
+    var_dump('Smaller');
+}
 
-$test = [ 
-    "name " => 'Birgithe', 
-    'age' => 18,
-    1,
-    3,
-    'bdasihbdhajs',
-    100=> 'new value',
-    5,
-];
-var_dump($test['name']);
-var_dump($test);
-$test = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-];
-var_dump($test[1][2]); 
+if($num > 5) {
+    var_dump('Bigger');
+} elseif($num == 5) {
+    var_dump('Smaller');
+} else {
+    var_dump('Equal');
+}
+
+$i = 1; 
+
+switch($num) {
+    case 1:
+        var_dump('yx');
+        break;
+    case 2:
+        var_dump('kaks');
+        break;
+        case 3:
+        var_dump('kolm');
+        break;
+    case 4:
+        var_dump('kolm neli');
+        break;
+    default:
+        var_dump('dunno');
+}
