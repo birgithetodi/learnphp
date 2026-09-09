c096bd17e357fa5a91a3823adc28cb852fc5af87
Birgithe Todi <birgithe.todi@tptlive.ee>
2026-09-09 18:31:35 +0300

    add trait example

--- index.php
+++ index.php
@@ -1,48 +1,21 @@
-<?php
+class Cat {
+use MakesSound;
+}
  
-class Box {
-    public int $width;
-    private int $height;
-    protected int $length;
-    public $isOpen = false;
-    public $hasBeenOpened = false;
+class Dog {
+use HasSmell;
+}
  
-     public function open() {
-        $this->isOpen = true;
-        $this->hasBeenOpened = true;
-    }
- 
-    public function close() {
-        $this->isOpen = false;
-    }
-    public function volume() {
-        return $this->height * $this->length * $this->width;
+trait hasSSmell {
+    public $smell;
+    public function sniff() {
+        return 'smells like' . $this->smell;
     }
 }
  
-class MetalBox extends Box {
-public $weight;
- 
- 
-public function mass(){
-return $this->volume() * $this->weight;
-    }
- 
-    public function test() {
-var_dump($this->length);
-    }
- 
-public function setHeight($height) {
-if(height > 0) {
-    $this->height = height;
-}
-    }
-    public function getHeight() {
-        return $this->height;
+trait MakesSound {
+    public $sound;
+    public function noise() {
+        return 'sounds like' . $this->sound;
     }
 }
- 
-$metal1 = new MetalBox();
-var_dump($metal1->width);
-$metal1->height = 1;
-var_dump($metal1);
