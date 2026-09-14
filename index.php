f09e32344e8e05002651b96cbab332daef36e485
Birgithe Todi <birgithe.todi@tptlive.ee>
2026-09-14 12:56:26 +0300

    add bootstrap blog template + Split blog template into partials

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
-<?php
+<?php include __DIR__ . '/partials/header.php'; ?>
 
-$sum = 0;
+    <main class="container">
+      <?php include __DIR__ . '/partials/hero.php'; ?>
+      <?php include __DIR__ . '/partials/featured.php'; ?>
 
-for($i=1;$i<1000;$i++) {
-   if($i%3 === 0 || $i%5 === 0) {
-        $sum+=$i;
-   } 
-}
-var_dump($sum);
+      <div class="row g-5">
+        <div class="col-md-8">
+          <?php include __DIR__ . '/partials/posts.php'; ?>
+        </div>
+        <div class="col-md-4">
+          <?php include __DIR__ . '/partials/sidebar.php'; ?>
+        </div>
+      </div>
+    </main>
+  <?php include __DIR__ . '/partials/footer.php'; ?>
