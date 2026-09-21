9dc1e4a759950a8572948070b1987d94fa3bb322
Birgithe Todi <birgithe.todi@tptlive.ee>
2026-09-21 15:54:54 +0300

    Add method type check to Router

--- public/index.php
+++ public/index.php
@@ -3,17 +3,17 @@ if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
     return false;    // serve the requested resource as-is.
 }
 
+
 spl_autoload_register(function ($class){
     $class = substr($class,4);
     $class = str_replace('\\', '/', $class);
     require_once __DIR__ . "/../src/$class.php";
 });
 
-
 require __DIR__ . '/../helpers.php';
 require __DIR__ . '/../routes.php';
 
-$router = new App\Router($_SERVER['REQUEST_URI']);
+$router = new App\Router($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
 $match = $router->match();
 if($match) {
     if(is_callable($match['action'])) {
